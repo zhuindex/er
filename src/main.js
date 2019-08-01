dc42d84c0552ed1c09418e37a6f42f0a11daf63f
朱春磊 <854348997@qq.com>
2019-08-01 20:49:17 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 控制台提示信息 开发环境来提示 详细一些
 Vue.config.productionTip = false
 
+// main.js的职责
+// 1. 导入项目需要依赖的资源（js包，css文件，其他资源）
+// 2. 创建一个根实例
+
+// 创建一个根实例 mount是挂载的意思
 new Vue({
   render: h => h(App)
 }).$mount('#app')
